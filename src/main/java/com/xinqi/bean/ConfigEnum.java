5128df6db1749078a447750d1bc7cdce0e7df212
XinQi <592342403@qq.com>
2023-03-16 04:18:15 +0800

    修改配置文件内容

--- src/main/java/com/xinqi/bean/ConfigEnum.java
+++ src/main/java/com/xinqi/bean/ConfigEnum.java
@@ -9,10 +9,10 @@ public enum ConfigEnum {
 
     CHATGPT_API("chatgpt_api",null),
     TELEGRAM_BOT_TOKEN("telegram_bot_token",null),
-    START_MSG("start_msg","已新建 ChatGPT 对话，你可以使用 /response 重新回到上一次对话"),
+    START_MSG("start_msg","已新建 ChatGPT 对话，你可以使用 /response 重新回到上一次对话（该消息由 TelegramBot 发出）"),
     CONFIGURE_DEFAULT_MENU("configure_default_menu",false),
     WHITELIST("whitelist",Collections.singletonList("*")),
-    NOT_WHITELIST_MSG("not_whitelist_msg","你不在白名单内，无法使用机器人"),
+    NOT_WHITELIST_MSG("not_whitelist_msg","你不在白名单内，无法使用机器人（该消息由 TelegramBot 发出）"),
     CHATGPT_ERROR_MSG("chatgpt_error_msg","ChatGPT 响应失败，请使用 /response 重试，若频繁出现此问题，请联系管理员（该消息由 TelegramBot 发出）"),
     END_MSG("end_msg","从上一条 ChatGPT 回复开始，已有段时间未使用 ChatGPT，已自动删除 ChatGPT 对话数据，如需要继续上一次对话请发送 /history（该消息由 TelegramBot 发出）"),
     HISTORY_MSG("history_msg","已回到上一次对话（该消息由 TelegramBot 发出）"),
