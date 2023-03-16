e1c6ec3e1786d2f689c896a67c660da52498e5f7
XinQi <592342403@qq.com>
2023-03-16 23:27:43 +0800

    更新日志位置

--- src/main/java/com/xinqi/api/TelegramBotApi.java
+++ src/main/java/com/xinqi/api/TelegramBotApi.java
@@ -45,26 +45,25 @@ public class TelegramBotApi {
             return null;
         }
 
-        //更新偏移量
+        //设置偏移量
         JsonNode resultData = result.get(0);
         //将收到消息的第一个update_id设置为偏移量并再次获取该重复消息以便将该消息偏移去除
         if (update_id == 0) {
             update_id = resultData.get("update_id").asInt();
             return getUpdates(botApi, logger);
         }
-        update_id++;
 
-        //得到其他所需数据
+        //判断是否是新消息，判断完后更新偏移值
         JsonNode resultMessage = resultData.get("message");
         //判断是否是新消息，如果是编辑的消息json数据里是edited_message，这里不处理编辑的消息
         if (resultMessage == null){
-            logger.info("循环调用 TelegramBot API 得到编辑的消息，将跳过该消息，请求地址: {}，接口返回内容为: {}，当前 update_id 为: {}", url, jsonNode, update_id);
+            logger.info("循环调用 TelegramBot API 得到编辑的消息，将跳过该消息，请求地址: {}，接口返回内容为: {}，当前 update_id 为: {}", url, jsonNode, update_id++);
             return null;
         }
-        logger.info("循环调用 TelegramBot API 得到新消息，请求地址: {}，接口返回内容为: {}，当前 update_id 为: {}", url, jsonNode, update_id);
-        JsonNode resultFrom = resultMessage.get("from");
+        logger.info("循环调用 TelegramBot API 得到新消息，请求地址: {}，接口返回内容为: {}，当前 update_id 为: {}", url, jsonNode, update_id++);
 
         //获取配置文件里的白名单列表
+        JsonNode resultFrom = resultMessage.get("from");
         String chatId = resultFrom.get("id").asText();
         @SuppressWarnings("unchecked") List<String> whitelist = (List<String>) ConfigEnum.WHITELIST.getValue();
         if (!whitelist.contains("*") && !whitelist.contains(chatId)) {
